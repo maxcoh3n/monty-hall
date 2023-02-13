bdf4484ce250edfc9cb86241a106e61aeb149d3f
maxcoh3n <maxcoh3n@gmail.com>
2023-02-13 14:16:01 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+The Monty Hall Problem is something that interests me a lot.
+
+[You can read about it more here](https://en.wikipedia.org/wiki/Monty_Hall_problem), but basically, the idea is that on the show, there are 3 doors. One contains a prize,
+and the other 2 contain goats. You select a door, and the host reveals another door with a goat, and asks if you want to swap doors. 
+According to mathemeticians, if you don't swap, the chance is 1/3. If you do swap, the chance is 2/3.
+
+The way I have coded it up here in the default case corroborates this claim. 
+You can try for yourself, or let the program run 1000 trials by itself either always choosing the swap strategy, or never choosing the swap strategy.
+However, what happens if the host is more mischevious? After all, these game shows are often rigged for contestants to fail.
+In the original pitch for the Problem, there is no guarantee that the host was going to reveal a door in every case. I.e. in the case that your original door contains a goat, or a prize.
+
+So, in this program, I have also included an option to play against an evil host, which the original problem doesn't preclude the existence of.
+Play a few rounds for yourself, and you'll see what the issue is. I guarantee that there's no coding tricks to sway probabilities, everything works exactly like it would
+in an actual game show.
+
+To try for yourself, download the code and simply run python3 ./monty-hall.py in the same directory as the program. 
---
+++ monty-hall.py
@@ -0,0 +1,121 @@
+import random
+from random import choice
+
+def print_doors(selection = [], goat=[], win = []):
+    ans = ["?","?","?"]
+    selection_list =  ["","",""]
+    
+    for door in win:
+        ans[door] = "$" 
+    if len(win) > 0:
+        for i in range(len(ans)):
+            if i not in win:
+                ans[i] = "X"
+    else:
+        for door in goat:
+            ans[door] = "X"  
+            
+    print("|{}| |{}| |{}|\n 0   1   2".format(ans[0],ans[1],ans[2]))
+
+    if len(selection) > 0:
+        selection_list[selection[0]] = "^"
+        print("{}   {}   {}".format(selection_list[0],selection_list[1],selection_list[2]))
+
+
+def monty_hall_helper_manual(evil = False):
+    wins = 0
+    trials = 0
+    while True:
+        doors = [0,0,0]
+        prize = random.randint(0,2)
+        doors[prize] = 1
+        print_doors()
+        while True:
+            selection = input("Select door 0,1, or 2\n")
+            try:
+                selection = int(selection) 
+            except:
+                print("invalid selection.")
+                continue
+            if selection < 0 or selection >2:
+                continue
+            else:
+                break
+        reveal = choice([i for i in range(0,3) if i not in [selection, prize]])
+        print_doors(selection = [selection])
+        print("The prize is not behind door {}".format(reveal))
+        print_doors(selection = [selection], goat=[reveal])
+        if reveal + selection == 1:
+            remaining = 2
+        elif reveal + selection == 2:
+            remaining = 1
+        elif reveal + selection == 3:
+            remaining = 0
+        else:
+            print("error")
+        while True:
+            val = input("Do you want to swap to door {} ? y/n ".format(remaining))
+            if val == "y" or val =="yes":
+                selection = remaining
+                break
+            elif val =="n" or val =="no":
+                break
+            else:
+                print("invalid response.")
+        if selection == prize:
+            print("\nYOU WON!!! Prize was in door {} ".format(prize))
+            wins +=1
+        else:
+            print("Sorry, you lost. Prize was in door {} " .format(prize))
+            print_doors(win=[prize], goat=[selection,reveal])
+        print_doors(selection = [selection], win=[prize])
+        trials +=1
+        val = input("play again? y/n ")
+        if val == "y" or val =="yes":
+            continue
+        else:
+            return (wins,trials)
+
+def monty_hall_helper_auto(swap):
+    wins = 0
+    trials = 0
+    for i in range(1000):
+        doors = [0,0,0]
+        prize = random.randint(0,2)
+        doors[prize] = 1
+        selection = random.randint(0,2)
+        reveal = choice([i for i in range(0,3) if i not in [selection, prize]])
+        if reveal + selection == 1:
+            remaining = 2
+        elif reveal + selection == 2:
+            remaining = 1
+        elif reveal + selection == 3:
+            remaining = 0
+        val = swap
+        if swap:
+            selection = remaining            
+        if selection == prize:
+            wins +=1
+        trials +=1
+    return (wins,trials)
+    
+def monty_hall(): 
+    style = input("would you like to play manually or automatically?")
+    if style =="m" or style =="man" or style =="manual" or style =="manually":
+        win, trials = monty_hall_helper_manual()
+    else:
+        swap = input("Do you want the computer to swap when prompted? y/n")
+        if swap == "y" or swap =="yes":
+            win, trials = monty_hall_helper_auto(True)
+        else:
+            win, trials = monty_hall_helper_auto(False)
+
+    
+    if(trials > 0):    
+        print("You won : {} out of {} times, or {} ".format(win, trials, win/trials))
+    else:
+        print("You didn't play. :(")
+    
+    
+    
+monty_hall()
